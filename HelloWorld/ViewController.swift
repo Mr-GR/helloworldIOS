0918fcd70548f36c347e9c56efb91c485ec8cadd
Mr-GR <manny2144@icloud.com>
2024-02-27 21:56:48 -0500

    Inital Comment

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,7 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var TxtName: UITextField!
+    
+    @IBOutlet weak var lblOutput: UILabel!
+    
+    @IBAction func showOutput(_ sender: Any) {
+        let name = TxtName.text
+        let output = "Hello " + name!
+        lblOutput.text = output
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
